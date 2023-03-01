35ba9d0bf963b75206078d62b8c7b7ad58b2e856
ejoelrojas9 <96266652+ejoelrojas9@users.noreply.github.com>
2023-03-01 10:44:27 -0500

    Inicio de las seccion 8

---
+++ seccion-8-sinatra/71-introduccion-a-la-seccion.rb
@@ -0,0 +1,19 @@
+Que es sinatra?
+
+- Es un framework para desarrollar aplicaciones web utilizando el lenguaje deprogramación Rubinary
+- Fue diseñado por Blake Mizerany en Calofirnia
+- Es mucho más sencillo de aprender que Ruby on Rails. Por lo tanto es
+excelente para que los principiantes se introduzcan al mundo del desarrollo
+web
+- Sitio oficial http://www.sinatradb.com/
+
+Que es lo que vamos aprender
+
+- Protocolo http
+    Funcionamiento basico
+    Métodos: Get, Post, Put, Delete
+- Bases del desarrollo web con sinatra
+- Routes
+- Templates
+- Parametros en la url
+- Como manejar información de archivos
